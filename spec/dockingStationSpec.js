69011f02605672df64411ffb7482b99531b4364b
Paul <paul.ayling@bglgroup.co.uk>
2019-02-07 10:36:39 +0000

    Updated release bike method to only release working bikes, added tests and refactored

--- spec/dockingStationSpec.js
+++ spec/dockingStationSpec.js
@@ -29,24 +29,35 @@ describe("DockingStation", function() {
       expect(dockingStation.bikes().length).toEqual(1);
     });
 
-    it("Should return an array of bikes that aren't broken", function() {
+    it("Should return an array of bikes that are working", function() {
       dockingStation.addBike(workingBike);
       dockingStation.addBike(brokenBike);
-      workingBikes = dockingStation.workingBikes();
-      expect(workingBikes.length).toEqual(1);
+      expect(dockingStation.workingBikes().length).toEqual(1);
     });
 
     it("Should prevent a bike from docking if station is at max capacity", function() {
       spyOn(dockingStation, 'maxCapacity').and.returnValue(true);
-      expect(function() { dockingStation.addBike(workingBike)}).toThrowError("Docking Station is at max capacity")
+      expect(function() {dockingStation.addBike(workingBike)}).toThrowError("Docking Station is at max capacity")
     });
   });
 
   describe("#releaseBike", function() {
 
-  it("Should release a bike fromt he dockingStation", function() {
+  it("Should release a bike from the dockingStation", function() {
     dockingStation.addBike(workingBike);
     expect(dockingStation.releaseBike()).toEqual(workingBike);
   });
+
+  it ("Should prevent a bike from being released if Docking Station has no working bikes", function() {
+    dockingStation.addBike(brokenBike);
+    expect(function() {dockingStation.releaseBike()}).toThrowError("No working bikes left")
+  })
+
+  it ("Should prevent a bike from being released if Docking Station is empty", function() {
+    spyOn(dockingStation, 'empty').and.returnValue(true);
+    expect(function() {dockingStation.releaseBike()}).toThrowError("Docking Station is empty")
+  });
+
+
   });
 }); //End of DockingStation
--- src/dockingStation.js
+++ src/dockingStation.js
@@ -11,6 +11,10 @@ DockingStation.prototype.maxCapacity = function() {
   return this._capacity === this._bikes.length;
 }
 
+DockingStation.prototype.empty = function() {
+  return this._bikes.length === 0;
+}
+
 DockingStation.prototype.bikes = function() {
   return this._bikes;
 }
@@ -25,12 +29,20 @@ DockingStation.prototype.checkBike = function(bike) {
 }
 
 DockingStation.prototype.workingBikes = function() {
-  allBikes = this.bikes();
-  var workingBikes = allBikes.filter(bike => bike.working());
+  var workingBikes = this.bikes().filter(bike => bike.working());
   // var workingBikes = allBikes.filter(function(bike) { return bike.working() });
   return workingBikes;
 }
 
 DockingStation.prototype.releaseBike = function() {
-  return this._bikes.shift();
+  if (this.empty()) { throw new Error("Docking Station is empty") }
+  var allBikes = this.bikes(); // Array containing all Bikes
+  var workingBikes = this.workingBikes(); // Array containing working bikes
+  if (workingBikes.length === 0) { throw new Error("No working bikes left") }
+  var releasedBike = workingBikes.shift(); // Released working bike
+  var index = allBikes.indexOf(releasedBike) // Index of released bike in all bike array
+  if (index > -1) {
+    allBikes.splice(index, 1); // Removed bike from all bikes array.
+  }
+  return releasedBike; // Return bike.
 }
